7ce99b8da22a55328e82e441c2d69a77f8565c6f
bobgel12 <bobgel12@gmail.com>
2018-01-25 10:52:47 -0800

    First change
    Init and outline

---
+++ app.js
@@ -0,0 +1,8 @@
+var express   = require("express"),
+  bodyParser  = require("body-parser"),
+  mongoose    = require("mongoose"),
+
+// models Post, Comment, User
+  Post        = require("./models/post")
+  Comment        = require("./models/comment")
+  User        = require("./models/user")
